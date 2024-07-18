e9e6b653ea485854079a362534ee4665d0a29d95
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-07-18 22:40:00 +0100

    infra update

--- shell.nix
+++ shell.nix
@@ -3,6 +3,9 @@ pkgs.mkShell {
   shellHook = ''export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
     pkgs.vulkan-loader
     pkgs.libxkbcommon
+    pkgs.wayland
+    pkgs.udev
+    pkgs.alsaLib
   ]}"'';
   buildInputs = with pkgs; [
     wayland
